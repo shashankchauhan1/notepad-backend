6b76b005a4ffd29d8faaf7a8de9107069a2645a4
shashank <shashank1049.be23@chitkara.edu.in>
2025-06-21 23:55:05 +0530

    ic

--- server.js
+++ server.js
@@ -15,14 +15,14 @@ app.use(express.json());
 app.use("/api/notes", noteRoutes);
 app.use("/api/auth", authRoutes); 
 
-mongoose
-  .connect(process.env.MONGO_URI)
-  .then(() => {
+mongoose.connect(process.env.MONGO_URI)
+.then(() => {
     console.log("Connected to MongoDB");
     app.listen(process.env.PORT || 7000, () => {
       console.log(`Server is running on port ${process.env.PORT || 7000}`);
     });
-  })
-  .catch((error) => {
+})
+.catch((error) => {
     console.error("MongoDB connection error:", error);
-  });
+});
+
